e4c05ede11d1816cf719eac666a86401909d905f
Jesus Bracho <jbrach347@gmail.com>
2019-12-25 20:50:35 -0500

    Visualize normals

--- src/ray.rs
+++ src/ray.rs
@@ -11,22 +11,30 @@ impl Ray {
     }
 
     pub fn color(&self) -> Vec3 {
-        if self.hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5) {
-            return Vec3(1.0, 0.0, 0.0);
-        }
+        let t = self.hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5);
 
-        let direction = self.direction.norm();
-        let time = 0.5 * (direction.y() + 1.0);
-        Vec3(1.0, 1.0, 1.0) * (1.0 - time) + Vec3(0.5, 0.7, 1.0) * time
+        if t > 0.0 {
+            let normal = (self.point_at(t) - Vec3(0.0, 0.0, -1.0)).norm();
+            Vec3(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0) * 0.5
+        } else {
+            let direction = self.direction.norm();
+            let time = 0.5 * (direction.y() + 1.0);
+            Vec3(1.0, 1.0, 1.0) * (1.0 - time) + Vec3(0.5, 0.7, 1.0) * time
+        }
     }
 
-    pub fn hit_sphere(&self, center: Vec3, radius: f32) -> bool {
+    pub fn hit_sphere(&self, center: Vec3, radius: f32) -> f32 {
         let oc = self.origin - center;
         let a = self.direction.dot(self.direction);
         let b = oc.dot(self.direction) * 2.0;
         let c = oc.dot(oc) - radius * radius;
         let discriminant = b * b - 4.0 * a * c;
-        discriminant > 0.0
+
+        if discriminant < 0.0 {
+            -1.0
+        } else {
+            (-b - discriminant.sqrt()) / (2.0 * a)
+        }
     }
 
     pub fn point_at(&self, time: f32) -> Vec3 {
