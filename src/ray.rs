aa0e6ad66eaac775dc25f4e071431814b2629993
Jesus Bracho <jbrach347@gmail.com>
2019-12-25 20:31:58 -0500

    Visualize intersections with a hard coded sphere

--- src/ray.rs
+++ src/ray.rs
@@ -11,9 +11,22 @@ impl Ray {
     }
 
     pub fn color(&self) -> Vec3 {
+        if self.hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5) {
+            return Vec3(1.0, 0.0, 0.0);
+        }
+
         let direction = self.direction.norm();
         let time = 0.5 * (direction.y() + 1.0);
         Vec3(1.0, 1.0, 1.0) * (1.0 - time) + Vec3(0.5, 0.7, 1.0) * time
+    }
+
+    pub fn hit_sphere(&self, center: Vec3, radius: f32) -> bool {
+        let oc = self.origin - center;
+        let a = self.direction.dot(self.direction);
+        let b = oc.dot(self.direction) * 2.0;
+        let c = oc.dot(oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
     }
 
     pub fn point_at(&self, time: f32) -> Vec3 {
